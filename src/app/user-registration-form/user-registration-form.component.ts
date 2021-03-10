1e0a436bfa5a2c1a3d495a094c07a14d13f165d8
Christopher Frenning <christopher@frenning.com>
2021-03-10 21:12:40 +0000

    UI Updates

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormControl } from '@angular/forms';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration-form',
@@ -9,13 +9,39 @@ import { Validators, FormControl } from '@angular/forms';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  name = new FormControl('', [Validators.required]); //, [new MinecraftAliasValidator().validate]);
-  alias = new FormControl('', [Validators.required]);
-  termsAccepted = new FormControl('');
+  form = new FormGroup( { 
+    name: new FormControl('', [Validators.required]), //, [new MinecraftAliasValidator().validate]);
+    alias: new FormControl('', [Validators.required]),
+    termsAccepted: new FormControl('', [Validators.required])
+  });
+
+  showTerms = false;
+  successfullyRegistered = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  async validate() {
+    const response = await fetch(`https://nji8ggmq.azurewebsites.net/api/MinecraftValidateUsername?name=${this.form.controls['name'].value}`);
+    console.log(response.body);
+  }
+
+  submit() {
+    //const response = await fetch(`https://nji8ggmq.azurewebsites.net/api/MinecraftValidateUsername?name=${this.form.controls['name'].value}`);
+    //console.log(response.text);
+    this.successfullyRegistered = true;
+  }
+
+  toggleTerms() {
+    this.showTerms = !this.showTerms;
+  }
+
+  reset() {
+    this.form.controls['name'].setValue("");
+    this.showTerms = this.successfullyRegistered = false;
   }
 
 }
